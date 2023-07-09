b806d17c76340f836135b31c40f65d535f162a62
Arpit Trikha <arpit_trikha05@yahoo.com>
2023-07-08 20:21:49 -0400

    node modules moved to .gitignore

---
+++ src/db.js
@@ -0,0 +1,11 @@
+const github = {
+    baseUrl: 'https://api.github.com/graphql',
+    username: 'arpit-05',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'bearer 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',
+    },
+  };
+  
+  export default github;
+  
